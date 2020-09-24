f23b03cdf5aa8c511f1676a8e7f312967952b5c1
sweetpotatoman <851951875@qq.com>
2020-09-24 11:51:28 +0800

    add raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    if not isinstance(number, int):
+        raise Exception("ERROR, input must be an integer!")
+    sound = ""
+    if number % 3 == 0:
+        sound += "Pling"
+    if number % 5 == 0:
+        sound += "Plang"
+    if number % 7 == 0:
+        sound += "Plong"
+    return sound if sound else str(number)
