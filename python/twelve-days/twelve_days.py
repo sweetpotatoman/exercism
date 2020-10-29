4efc24b02177cc8915a3a19c751f3d6101429d72
sweetpotatoman <851951875@qq.com>
2020-10-29 18:43:30 +0800

    temp

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,32 @@
+DAYS = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+]
+
+GIFTS = [
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
+]
+
+def recite(start_verse, end_verse):
+    pass
