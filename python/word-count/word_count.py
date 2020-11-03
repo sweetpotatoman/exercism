3b660a31b9c757dc3eb9cb011b2a63914b6a3fcf
sweetpotatoman <851951875@qq.com>
2020-11-03 15:04:11 +0800

    upload twelve-days/word_count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,10 @@
+from re import findall
+from collections import Counter
+
+
 def count_words(sentence):
-    pass
+    word = findall(r"[a-zA-Z0-9]+'\w+|[a-zA-Z0-9]+", sentence.lower())
+    return Counter(word)
+
+print(count_words("First: don're laugh. Then: don're cry."))
+print(count_words("''hey'' 1234 ''``hjf``f9010''"))
