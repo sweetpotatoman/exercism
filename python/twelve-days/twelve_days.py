8de23c996aad31fb23d4e4b6dea77ea6bb295429
sweetpotatoman <851951875@qq.com>
2020-11-06 18:46:02 +0800

    unfinished work before adding

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,32 +1,30 @@
-DAYS = [
-    "first",
-    "second",
-    "third",
-    "fourth",
-    "fifth",
-    "sixth",
-    "seventh",
-    "eighth",
-    "ninth",
-    "tenth",
-    "eleventh",
-    "twelfth"
-]
-
-GIFTS = [
-    "a Partridge in a Pear Tree",
-    "two Turtle Doves",
-    "three French Hens",
-    "four Calling Birds",
-    "five Gold Rings",
-    "six Geese-a-Laying",
-    "seven Swans-a-Swimming",
-    "eight Maids-a-Milking",
-    "nine Ladies Dancing",
-    "ten Lords-a-Leaping",
-    "eleven Pipers Piping",
-    "twelve Drummers Drumming"
-]
-
 def recite(start_verse, end_verse):
-    pass
+    days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
+
+    lines = ["a Partridge in a Pear Tree.", 
+            "two Turtle Doves, ",
+            "three French Hens, ",
+            "four Calling Birds, ",
+            "five Gold Rings, ",
+            "six Geese-a-Laying, ",
+            "seven Swans-a-Swimming, ",
+            "eight Maids-a-Milking, ",
+            "nine Ladies Dancing, ",
+            "ten Lords-a-Leaping, ",
+            "eleven Pipers Piping, ",
+            "twelve Drummers Drumming, "]
+
+
+    song = []
+    for i in range(start_verse-1, end_verse):
+        verse = f"On the {days[i]} day of Christmas my true love gave to me: "
+        for j in range(i, 0, -1):
+            verse += lines[j]
+        if i != 0:
+            verse += "and "
+        verse += lines[0]
+        song.append(verse)
+
+    return song
+
+print(recite(1,1))
